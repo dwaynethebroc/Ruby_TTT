0dc188e49ffdf116a029f5608e742da4c9b9f825
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-09-09 16:45:14 -0700

    game turn works, need to make win/tie check

--- program.rb
+++ program.rb
@@ -3,17 +3,28 @@ class Game
   @@total_losses = 0
   @@total_ties = 0
 
-  def self.check_win
-  
-  end
+  @@win? = false
+  @@tie? = false
 
-  def self.game_turn
+  def self.check_win_tie(board)
+    row = i
+    col = j
+
+    if row
+
+
+  def self.game_turn(player1, player2, board)
 
     player1.choose_placement(board)
-    board.display_board
+    check_win_tie(board)
+
 
     player2.choose_placement(board)
-    board.display_board
+    check_win_tie(board)
+
+    if @@win? == false && @@tie == false
+      game_turn(player1, player2, board)
+    end
   end
 end
 
@@ -30,17 +41,28 @@ class Player < Game
   end
 
   def choose_placement(game_board)
-    'Choose a number between 1-9 to choose index'
-    player_choice = gets.chomp.to_i - 1
+
+    game_board.display_board
+
+    input = nil
+    
+    until input =~ /^[1-9]$/
+      puts 'Choose a number between 1-9 to choose index: '
+      input = gets.chomp
+    end
+
+    player_choice = input.to_i - 1
 
     row = (player_choice / 3).floor
     column = (player_choice % 3).floor
 
-    if @board[row][column] == '-'
+    if player_choice.between?(0, 8) && game_board.board[row][column] == '-'
       game_board.update_board(player_choice, @token)
       game_board.display_board
-    end 
-    
+    else
+      puts "Invalid position or position already taken. Choose another."
+      choose_placement(game_board)
+    end
   end
 end
 
@@ -75,4 +97,4 @@ player2 = Player.new('Player 2', 'O')
 
 board = Board.new
 
-player1.choose_placement(board)
+Game.game_turn(player1, player2, board)
